f336ff782220863478ad5f54212cee41a1849b1f
BitEU <45499599+BitEU@users.noreply.github.com>
2025-11-23 17:20:17 -0500

    Added header to files

--- dinit.c
+++ dinit.c
@@ -4,6 +4,40 @@
 /* ALL RIGHTS RESERVED, COMMERCIAL USAGE STRICTLY PROHIBITED */
 /* WRITTEN BY R. M. SUPNIK */
 
+/*
+ * Program Name: Zork
+ * Program Release Year: 2025
+ * Program Author: Steven S.
+ * Program Link: https://github.com/BitEU/Zork
+ * 
+ * Original Name: Zork
+ * Original Release Year: 1977
+ * Original Author: Tim Anderson, Marc Blank, Bruce Daniels, and Dave Lebling
+ * Original Link: N/A, but a good article exists here https://www.technologyreview.com/2017/08/22/149560/the-enduring-legacy-of-zork/
+ * Original System: PDP-10
+ * 
+ * Rewrite Name: Zork I
+ * Rewrite Release Year: 1991
+ * Rewrite Author: Ian Lance Taylor, devshane, ChandlerSwift, jaredledvina, justwheel, and Jan200101
+ * Rewrite Link: https://github.com/devshane/zork
+ * 
+ * Extensive version history:
+ *  11-NOV-25  Modified to run on UNIVAC 1219
+ * 11-MAR-91	Converted to C (V2.7)
+ * 14-SEP-87	Converted to f77/Unix for pdps and Vaxen (V2.6B)
+ * 18-JUL-80	Transportable data base file (V2.5A).
+ * 28-FEB-80	Compressed text file (V2.4A).
+ * 15-NOV-79	Bug fixes (V2.3A).
+ * 18-JAN-79	Revised DECUS version (V2.2A).
+ * 10-OCT-78	Puzzle Room (V2.1A).
+ * 10-SEP-78	Endgame (V2.0A).
+ * 10-AUG-78	DECUS version (V1.1B).
+ * 14-JUN-78	Public version with parser (V1.1A).
+ * 4-MAR-78	Debugging version (V1.0A).
+ *
+ * Major changes from rewrite: Added UNIVAC 1219 support
+ */
+
 #include <stdio.h>
 
 #ifdef __AMOS__
